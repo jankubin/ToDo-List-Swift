bcf041fc7faefd635d545407b0abcfe8ebe56743
jankubin <101505317+jankubin@users.noreply.github.com>
2023-05-30 14:29:20 +0200

    1

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,24 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Jan Kubín on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationView{
+                ListView()
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .environmentObject(listViewModel)
+        }
+    }
+}
